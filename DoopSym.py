09aac7035497ea9f21483654fc6c503fdde5c436
Doug Simmons <etc@302.nyc>
2015-12-16 13:33:44 -0500

    Create DoopSym.py

---
+++ DoopSym.py
@@ -0,0 +1,15 @@
+# This is a fork of Doopy, https://github.com/philipforget/Doop
+# Doop detects duplicate images, and I want to extend it to let
+# the user delete duplicate images while replacing the deleted
+# files with symbolic links to the other and still-existing 
+# detected file. 
+# 
+# The purpose I have in mind is for a very image-heavy
+# website with tens of thousands of images, to be able to free 
+# up duplicates without resulting in many of your pages
+# having broken images due to the symbolic linking.
+# 
+# Guess I'll call it DoopSym.
+# 
+# Hmm.. 
+ln -s yada yoda ?
